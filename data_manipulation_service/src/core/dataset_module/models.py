37aa8791d9067d223d56e1a8dc0f11f9ad3474e0
SindiK_Aspire <sindereve850live@gmail.com>
2026-01-13 02:35:57 +0500

    feat: create domain models

--- data_manipulation_service/src/core/dataset_module/models.py
+++ data_manipulation_service/src/core/dataset_module/models.py
@@ -6,3 +6,22 @@ class FileSystemManagerStatus:
     dataset: Optional[str]
     version: Optional[str]
     dataset_class: Optional[str]
+
+@dataclass
+class ClassInfo:
+    name: str 
+    description: Optional[str]
+    count_files: Optional[int]
+    type_files: Optional[str]
+
+@dataclass
+class VersionInfo:
+    name: str
+    description: Optional[str]
+    classes: list[ClassInfo]
+
+@dataclass
+class DatasetInfo:
+    name: str
+    description: Optional[str]
+    versions: list[VersionInfo]
