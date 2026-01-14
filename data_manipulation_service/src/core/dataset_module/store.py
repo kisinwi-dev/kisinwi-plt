d57dc314736a3f73c7c8aa9f02981e94be3b46fd
SindiK_Aspire <sindereve850live@gmail.com>
2026-01-14 16:43:51 +0500

    feat: new metod in Store

--- data_manipulation_service/src/core/dataset_module/store.py
+++ data_manipulation_service/src/core/dataset_module/store.py
@@ -1,4 +1,5 @@
-from core.dataset_module.filesystem import FileSystemManager
+from .filesystem import FileSystemManager
+from .models import ClassInfo, VersionInfo, DatasetInfo
 
 class Store:
     def __init__(
@@ -12,13 +13,13 @@ class Store:
         dataset_list = self._fsm.get_all_dir()
         return dataset_list
 
-    def get_dataset_version(self, dataset_name: str) -> list[str]:
+    def get_dataset_version_name(self, dataset_name: str) -> list[str]:
         self._fsm.reset()
         self._fsm.in_dir(dataset_name)
         version_list = self._fsm.get_all_dir()
         return version_list        
 
-    def get_dataset_classes(
+    def get_dataset_classes_name(
             self, 
             dataset_name: str,
             dataset_version: str,
@@ -28,7 +29,7 @@ class Store:
         classes_list = self._fsm.get_all_dir()
         return classes_list
     
-    def get_dataset_files(
+    def get_dataset_files_name(
             self,
             dataset_name: str,
             dataset_version: str,
@@ -38,8 +39,45 @@ class Store:
         self._fsm.in_dirs([dataset_name, dataset_version, class_name])
         return self._fsm.get_all_file()
     
-    def get_dataset_info(self):
-        pass
+    def get_dataset_info(
+            self,
+            dataset_name: str
+        ):
+        self._fsm.reset()
+        self._fsm.in_dir(dataset_name)
+        versions = self._fsm.get_all_dir()
+        info_versions = []
+        for version in versions:
+            self._fsm.in_dir(version)
+            classes = self._fsm.get_all_dir()
+            info_classes = []
+            for class_ in classes:
+                self._fsm.in_dir(class_)
+                files = self._fsm.get_all_file()
+                info_classes.append(
+                    ClassInfo(
+                        name=class_,
+                        description=None,
+                        count_files=len(files),
+                        type_files=None
+                    )
+                )
+                self._fsm.out_dir()
+            
+            info_versions.append(
+                VersionInfo(
+                    name=version,
+                    description=None,
+                    classes=info_classes
+                )
+            )
+            self._fsm.out_dir()
+        
+        return DatasetInfo(
+            name=dataset_name,
+            description=None,
+            versions=info_versions
+        )
 
     def set_new_dataset(self):
         pass
