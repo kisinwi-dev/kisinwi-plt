9f7fd38f870ff0a3671af35cf4685b5d324df0de
SindiK_Aspire <sindereve850live@gmail.com>
2026-01-16 00:03:30 +0500

    feat: api get info dataset

--- data_manipulation_service/src/api/routers/dataset.py
+++ data_manipulation_service/src/api/routers/dataset.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from core.dataset_module.store import Store
 from api.deps import get_store
@@ -14,3 +14,20 @@ def list_datasets(
         store: Store = Depends(get_store)
     ):
     return store.get_dataset_name()
+
+@router.get(
+    "/datasets/{dataset_name}",
+    summary="Get dataset info",
+)
+def info_dataset(
+        dataset_name: str,
+        store: Store = Depends(get_store)
+    ):
+    try:
+        info = store.get_dataset_info(dataset_name)
+        return info
+    except FileNotFoundError as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e),
+        )
