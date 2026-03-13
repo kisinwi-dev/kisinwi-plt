d748a38a74b5ffa6e5a1ed0226e24fa3fa386dcf
sindik <sindereve850live@gmail.com>
2026-03-13 12:47:14 +0500

    feat(dms): version exception

---
+++ data_manipulation_service/app/core/exception/version.py
@@ -0,0 +1,31 @@
+from .base import CoreException
+
+class VersionNotFoundError(CoreException):
+    """Версия не найден"""
+    
+    def __init__(self, version_id: str):
+        super().__init__(
+            message=f"Версия с идентификатором '{version_id}' не найдена",
+            status_code=404,
+            detail="Убедитесь, что идентификатор указан верно."
+        )
+
+class VersionValidationError(CoreException):
+    """Структура или содержимое версии не соответствует требованиям"""
+    
+    def __init__(
+            self, 
+            reason: str, 
+            version_id: str | None = None
+    ):
+        msg = f"Ошибка валидации датасета"
+        
+        if version_id:
+            msg += f" '{version_id}'"    
+        msg += f": {reason}"
+        
+        super().__init__(
+            message=msg,
+            status_code=400,
+            detail=reason
+        )
