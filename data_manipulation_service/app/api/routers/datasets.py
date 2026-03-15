284af0cfda328a0619938eedb988b484da5d797d
sindik <sindereve850live@gmail.com>
2026-03-15 20:58:11 +0500

    feat(api): add routers datasets

---
+++ data_manipulation_service/app/api/routers/datasets.py
@@ -0,0 +1,72 @@
+from fastapi import APIRouter, Depends, UploadFile, File
+from typing import List
+
+from app.logs import get_logger
+from app.core.filesystem import ArchiveManager
+from app.core.services.dataset import DatasetManager
+from app.api.deps import get_dataset_manager
+from app.api.schemas.dataset import DatasetMetadata
+from app.api.schemas.dataset_new import NewDataset
+
+logger = get_logger(__name__)
+router = APIRouter(prefix="/datasets", tags=["Datasets"])
+
+@router.get(
+    "/", 
+    response_model=List[str],
+    summary="Получение списка датасетов",
+    description="Возвращает список идентифакторов всех доступных датасето",
+    response_description="Список идентификатор",
+)
+def list_datasets(dm: DatasetManager = Depends(get_dataset_manager)):
+    return dm.get_datasets_id()
+
+@router.get(
+    "/{dataset_id}", 
+    response_model=DatasetMetadata,
+    summary="Получить метаданные датасета",
+    description="Возвращает метаданные указанного датасета по его идентификатору",
+    response_description="Метаданные датасета",
+)
+def get_dataset(dataset_id: str, dm: DatasetManager = Depends(get_dataset_manager)):
+    return dm.get_dataset_info(dataset_id)
+
+@router.delete(
+    "/{dataset_id}", 
+    response_model=bool,
+    summary="Удалить датасет",
+    description="Удаляет указанный датасет из системы",
+    response_description="True, если датасет был успешно удалён",
+)
+def delete_dataset(dataset_id: str, dm: DatasetManager = Depends(get_dataset_manager)):
+    return dm.drop_dataset(dataset_id)
+
+@router.post(
+    "/new/", 
+    response_model=bool,
+    summary="Создать датасет",
+    description="Создаёт новый датасет из загружает файл данных.",
+    response_description="True, если датасет успешно создан",
+)
+def create_dataset(
+    new_dataset: NewDataset, 
+    dm: DatasetManager = Depends(get_dataset_manager),
+):
+    dm.add_new_dataset(new_dataset)
+    return True
+
+@router.post(
+    "/upload",
+    response_model=bool,
+    summary="Загрузка данных датасета",
+    response_description="True, если датасет успешно загружен",
+)
+def uploads_data(
+    file: UploadFile = File(..., description="Файл датасета"),
+):
+    af = ArchiveManager()
+    save_path = af.save_file(file, str(file.filename))
+
+    _ = af.unpack(save_path, str(file.filename).split('.')[0])
+    return True
+
