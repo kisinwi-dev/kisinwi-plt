2ae72be68c6a48dafcc52cf0a8d6a3f664252d06
sindik <sindereve850live@gmail.com>
2026-03-16 12:15:53 +0500

    feat(routers): add version routers

---
+++ data_manipulation_service/app/api/routers/versions.py
@@ -0,0 +1,68 @@
+from fastapi import APIRouter, Depends, UploadFile, File
+from typing import List
+
+from app.logs import get_logger
+from app.core.filesystem import ArchiveManager
+from app.core.services.dataset import DatasetManager
+from app.api.deps import get_dataset_manager
+from app.api.schemas.dataset import Version
+from app.api.schemas.dataset_new import NewVersion
+
+logger = get_logger(__name__)
+router = APIRouter(prefix="/datasets/{dataset_id}", tags=["Version"])
+
+@router.get(
+    "/", 
+    response_model=List[Version],
+    summary="Получение списка версий",
+    description="Возвращает список всех доступных версий датасета",
+    response_description="Список версий",
+)
+def list_datasets(
+        dataset_id: str,
+        dm: DatasetManager = Depends(get_dataset_manager)
+):
+    return dm.get_dataset_info(dataset_id).versions
+
+@router.delete(
+    "/{version_id}", 
+    response_model=bool,
+    summary="Удалить версию",
+    description="Удаляет указанную версию датасета из системы",
+    response_description="True, если версия была успешно удалёна",
+)
+def delete_dataset(
+        dataset_id: str,
+        version_id: str,
+        dm: DatasetManager = Depends(get_dataset_manager)
+):
+    return dm.drop_version(dataset_id, version_id)
+
+@router.post(
+    "/new", 
+    response_model=bool,
+    summary="Добавить новую версию данных",
+    description="Создаёт новую версию датасета из загруженных данных",
+    response_description="True, если датасет успешно создан",
+)
+def create_dataset(
+    dataset_id: str,
+    new_dataset: NewVersion, 
+    dm: DatasetManager = Depends(get_dataset_manager),
+):
+    return dm.add_new_version(dataset_id, new_dataset)
+
+@router.post(
+    "/upload",
+    response_model=bool,
+    summary="Загрузка данных",
+    response_description="True, если версия успешно загружена",
+)
+def uploads_data(
+    file: UploadFile = File(..., description="Файл"),
+):
+    af = ArchiveManager()
+    save_path = af.save_file(file, str(file.filename))
+
+    _ = af.unpack(save_path, str(file.filename).split('.')[0])
+    return True
