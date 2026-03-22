39ff0ae54027ae88461c554a7f110e369aa93bc4
sindik <sindereve850live@gmail.com>
2026-03-22 20:09:34 +0500

    feat(ms): init

---
+++ metrics_service/app/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Dict, List, Union
+
+class MetricUpdate(BaseModel):
+    task_id: str
+    metric_name: str
+    value: Union[float, int, List[float]]
+    step: int
+
+class MetricData(BaseModel):
+    steps: List[int] = []
+    values: List[Union[float, int, List[float]]] = [] 
+
+class TaskMetrics(BaseModel):
+    task_id: str
+    metrics: Dict[str, MetricData] = {} 
+
+class MetricsResponse(BaseModel):
+    task_id: str
+    metrics: Dict[str, MetricData]
---
+++ metrics_service/main.py
@@ -0,0 +1,43 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+# from app.core.exception.base import CoreException
+from app.api import api_routers
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Data Manipulation Service",
+    version="0.1.0"
+)
+
+app.include_router(
+    api_routers,
+    prefix="/api"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.exception_handler(CoreException)
+# async def core_exception_handler(request: Request, exc: CoreException):
+#     return JSONResponse(
+#         status_code=exc.status_code,
+#         content={
+#             "error": exc.message,
+#             "detail": exc.detail
+#         }
+#     )
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=3000,
+        reload=True,
+    )
