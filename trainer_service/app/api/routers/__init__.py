b82cc4a18c7afd85b1f7a8d88055fb1f2fc84e5d
Sindereve <sindereve850live@gmail.com>
2026-04-08 17:51:28 +0500

    feat(ts): add get example train config

--- trainer_service/app/api/routers/__init__.py
+++ trainer_service/app/api/routers/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
+from .train_settings import routers as tr
 
 api_routers = APIRouter()
+
+api_routers.include_router(tr)
 
 @api_routers.get("/health")
 async def health_check():
