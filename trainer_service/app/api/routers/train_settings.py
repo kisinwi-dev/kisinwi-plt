15ee68749a7674cc23b5e75653c43437c6bd3916
Sindereve <sindereve850live@gmail.com>
2026-04-09 22:18:51 +0500

    feat: change structure in code style

--- trainer_service/app/api/routers/train_settings.py
+++ trainer_service/app/api/routers/train_settings.py
@@ -5,62 +5,63 @@ from app.core.task.classification.models import get_models_type_name
 
 routers = APIRouter()
 
+EXAMLE_TASK_CONFIG ={
+    "task_id": "cat_dog",
+    "data_loader_params": {
+        "dataset_id": "pet_img",
+        "version_id": "v_1",
+        "img_w_size": 224,
+        "img_h_size": 224,
+        "batch_size": 32
+    },
+    "model_params": {
+        "type": "Resnet",
+        "name": "resnet50",
+        "weights": True
+    },
+    "trainer_params": {
+        "loss_fn_config": {
+            "type": "CrossEntropyLoss",
+            "params": {
+                "reduction": "mean",
+                "label_smoothing": 0.1
+            }
+        },
+        "optimizer_config": {
+            "type": "AdamW",
+            "params": {
+                "lr": 0.0001,
+                "betas": (0.9, 0.999),
+                "eps": 1e-8,
+                "weight_decay": 0.05,
+                "amsgrad": False
+            }
+        },
+        "scheduler_config": {
+            "type": "CosineAnnealingLR",
+            "params": {
+                "T_max": 30,
+                "eta_min": 1e-6,
+                "last_epoch": -1
+            }
+        },
+        "device": "cuda",
+        "epochs": 30
+    }
+}
+
+
 @routers.get("/get_example_config")
 async def get_example_config() -> Dict[str, Any]:
     """
     Возвращает пример JSON конфигурации для обучения модели
     """
-    task_config_dict = {
-        "task_id": "cat_dog",
-        "data_loader_params": {
-            "dataset_id": "pet_img",
-            "version_id": "v_1",
-            "img_w_size": 224,
-            "img_h_size": 224,
-            "batch_size": 32
-        },
-        "model_params": {
-            "type": "Resnet",
-            "name": "resnet50",
-            "weights": True
-        },
-        "trainer_params": {
-            "loss_fn_config": {
-                "type": "CrossEntropyLoss",
-                "params": {
-                    "reduction": "mean",
-                    "label_smoothing": 0.1
-                }
-            },
-            "optimizer_config": {
-                "type": "AdamW",
-                "params": {
-                    "lr": 0.0001,
-                    "betas": (0.9, 0.999),
-                    "eps": 1e-8,
-                    "weight_decay": 0.05,
-                    "amsgrad": False
-                }
-            },
-            "scheduler_config": {
-                "type": "CosineAnnealingLR",
-                "params": {
-                    "T_max": 30,
-                    "eta_min": 1e-6,
-                    "last_epoch": -1
-                }
-            },
-            "device": "cuda",
-            "epochs": 30
-        }
-    }
-    
-    return task_config_dict
+    return EXAMLE_TASK_CONFIG
 
 
 @routers.get("/get_available_models")
 async def get_available_models():
     """
-    Возвращает пример JSON конфигурации для обучения модели
+    Возвращает пример JSON доступных типов и имён моделей
     """
     return get_models_type_name()
