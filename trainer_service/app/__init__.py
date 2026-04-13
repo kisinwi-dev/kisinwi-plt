cfd2bd9f031fde3cc0d2d5d892af3ddb423dae8d
Sindereve <sindereve850live@gmail.com>
2026-04-13 16:49:17 +0500

    refactor: init task the service

--- trainer_service/app/__init__.py
+++ trainer_service/app/__init__.py
@@ -6,8 +6,7 @@ from fastapi import FastAPI
 from .api.routers import api_routers
 from .core import training_model
 from .logs import get_logger
-from .service.tasker.tasker import Tasker_Service, TaskStatus
-from .config import config_domain
+from .service.tasker.tasker import tasker_service, TaskStatus
 
 logger = get_logger(__name__)
 
@@ -37,19 +36,16 @@ async def to_work():
     """
         Запуск воркера с постоянным опросом сервиса задач
     """
-
-    tasker_domen = config_domain.TASKER
-
     async with httpx.AsyncClient(timeout=30.0) as client:
         logger.info("Воркер начал работу")
         
         time_sleep = 1
-        ts = Tasker_Service(client)
+        tasker_service.set_client(client)
 
         while True:
 
             # Запрашиваем задачу
-            task = await ts.get_next_task()
+            task = await tasker_service.get_next_task()
 
             if task is None:
                 logger.debug(f"Повторный запрос через {time_sleep} сек.")
@@ -57,21 +53,21 @@ async def to_work():
                 continue
 
             task_id = task.task_id
-            config = task.payload
+            params = task.params
 
             logger.info(f"Worker начинает работу над задачей: {task_id}")
             
             # Обновение статуса задачи (выполняется)
-            await ts.update_status_task(task_id, TaskStatus.IN_PROGRESS, 0, "Задача принята воркером")
+            await tasker_service.update_status_task(TaskStatus.IN_PROGRESS, 0, "Задача принята воркером")
             
             try:
                 # Процесс обучения
-                training_model(task_id, config)
+                training_model(params)
 
                 # Завершение
-                await ts.update_status_task(task_id, TaskStatus.COMPLETED, 100, "Задача выполнена")
+                await tasker_service.update_status_task(TaskStatus.COMPLETED, 100, "Задача выполнена")
                 logger.info(f"Задача {task_id} завершена")
                 
             except Exception as e:
-                await ts.update_status_task(task_id, TaskStatus.FAILED, 0, "Задача завершена с ошибкой")
+                await tasker_service.update_status_task(TaskStatus.FAILED, 0, "Задача завершена с ошибкой")
                 logger.error(f"Ошибка {task_id}: {e}")
