e95cf4f73a6d1faff0361de08f6cf603c2f494e7
Sindereve <sindereve850live@gmail.com>
2026-04-20 20:16:33 +0500

    feat: added a router for receiving information about devices

--- trainer_service/app/api/routers/train_settings.py
+++ trainer_service/app/api/routers/train_settings.py
@@ -2,6 +2,7 @@ from typing import Dict, Any, List
 from fastapi import APIRouter, Query, HTTPException, status
 
 from app.logs import get_logger
+from app.core.utils import get_system_info as system_info
 from app.core.models import get_models_type_name
 from app.api.schemes import TaskParams
 
@@ -52,3 +53,23 @@ async def get_available_models(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Не удалось получить список доступных моделей: {e}"
         )
+
+@routers.get(
+        "/device",
+        response_model=Dict[str, Any],
+        summary="Получить информация о технических возможностях обучения"
+)
+async def get_system_info():
+    """
+    Возвращает список доступных вычислительных устройств
+    """
+    try:
+        si = system_info()
+        return si
+    except Exception as e:
+        logger.error(f"Ошибка при получении информации о технических возможностях обучения: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Не удалось получить список имеющихся вычислительных устройств: {e}"
+        )
+
--- trainer_service/test.py
+++
@@ -1,3 +0,0 @@
-from app.core.utils import get_system_info
-
-print(get_system_info())
