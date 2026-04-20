628e09b713d455fe80e4da261121b50c1c3a8500
Sindereve <sindereve850live@gmail.com>
2026-04-20 20:08:26 +0500

    refactor: improved the quality of processing requests for information

--- trainer_service/app/api/routers/train_settings.py
+++ trainer_service/app/api/routers/train_settings.py
@@ -1,20 +1,42 @@
-from typing import Dict, Any
-from fastapi import APIRouter, Query, HTTPException
+from typing import Dict, Any, List
+from fastapi import APIRouter, Query, HTTPException, status
 
+from app.logs import get_logger
 from app.core.models import get_models_type_name
 from app.api.schemes import TaskParams
 
-routers = APIRouter()
+logger = get_logger(__name__)
 
-@routers.get("/get_example_config")
+routers = APIRouter(
+    prefix='/info',
+    tags=['Information']
+)
+
+@routers.get(
+        "/example_config",
+        response_model=Dict[str, Any],
+        summary="Получить пример конфигураций для запуска обучения"
+)
 async def get_example_config() -> Dict[str, Any]:
     """
     Возвращает пример JSON конфигурации для обучения модели
     """
-    return TaskParams.model_json_schema()
+    try:
+        config_schema = TaskParams.model_json_schema()
+        return config_schema
+    except Exception as e:
+        logger.error(f"Ошибка при получении примера конфигруций: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Не удалось сгенерировать схему конфигурации: {e}"
+        )
 
 
-@routers.get("/get_available_models")
+@routers.get(
+        "/ml_models",
+        response_model=List[str],
+        summary="Получить список доступных ML моделей"
+)
 async def get_available_models(
     filter: str | None = Query(None, description="Фильтр для поиска моделей (например: '*resnet*')")
 ):
@@ -22,6 +44,11 @@ async def get_available_models(
     Возвращает список доступных моделей
     """
     try:
-        return get_models_type_name(filter)
+        models = get_models_type_name(filter)
+        return models 
     except Exception as e:
-        return HTTPException(status_code=500, detail=str(e))
+        logger.error(f"Ошибка при получении списка моделей: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Не удалось получить список доступных моделей: {e}"
+        )
