c6dc6c77077bb89537a3b360d0566c69c70d7fee
Sindereve <sindereve850live@gmail.com>
2026-04-20 21:09:29 +0500

    feat: api changes in the functions of requesting information from the training service

--- agent_office/app/services/trainer/__init__.py
+++ agent_office/app/services/trainer/__init__.py
@@ -1,25 +1,53 @@
 import os
 import requests
+from typing import Dict, Any, List
 from crewai.tools import tool
 
-TRAINER_URL = "http://" + os.getenv("TRAINER_DOMEN", "localhost:6200")
+from app.logs import get_logger
+from app.config import config_url
+
+logger = get_logger(__name__)
 
 @tool("GetExampleJSONTrainer")
-def get_example_run_config_trainer_json() -> str:
+def get_example_run_config_trainer_json() -> Dict[str, Any]:
     """Получить пример JSON для запуска тренировки модели"""
     try:
-        resp = requests.get(f"{TRAINER_URL}/get_example_config")
+        resp = requests.get(f"{config_url.TRAINER_URL}/info/example_config")
         data = resp.json()
         return data
     except Exception as e:
-        return f"Ошибка: {e}"
+        logger.error("Не удалось получить пример конфигураций обучения")
+        return {"error": e}
 
 @tool("GetAllAvailableModels")
-def get_type_and_name_models() -> str:
-    """Получить все имеющиеся модели в распоряжении. В зачениях json находится тип, а в списках имена"""
+def get_type_and_name_models(filter: str) -> List[str]:
+    """
+    Получить все имеющиеся модели в распоряжении.
+
+    Args:
+        filter - фильтр моделей
+    """
     try:
-        resp = requests.get(f"{TRAINER_URL}/get_available_models")
+        params={}
+        params["filter"] = filter
+
+        resp = requests.get(
+            f"{config_url.TRAINER_URL}/info/available_models",
+            params=params
+        )
         data = resp.json()
         return data
     except Exception as e:
-        return f"Ошибка: {e}"
+        logger.error("Не удалось получить информацию об имеющихся моделях")
+        return [f'Ошибка: {e}']
+
+@tool("GetInfoDevice")
+def get_info_device() -> Dict[str, Any]:
+    """Получить информацию о технических возможностях обучения"""
+    try:
+        resp = requests.get(f"{config_url.TRAINER_URL}/info/device")
+        data = resp.json()
+        return data
+    except Exception as e:
+        logger.error("Не удалось получить информацию об устройствах")
+        return {"error": e}
