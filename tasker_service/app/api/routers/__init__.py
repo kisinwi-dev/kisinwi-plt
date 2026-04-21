b0c24dcacf5d600e648ff065a70eadfa61a44fc8
Sindereve <sindereve850live@gmail.com>
2026-04-21 11:35:22 +0500

    feat: add router health

--- tasker_service/app/api/routers/__init__.py
+++ tasker_service/app/api/routers/__init__.py
@@ -4,3 +4,7 @@ from .task import routers as routers_task
 routers = APIRouter()
 
 routers.include_router(routers_task)
+
+@routers.get("/health")
+async def healt_check():
+    return {"status": "ok"}
