2db6d34390571662611412d5c3a33c631cd5f89d
Sindereve <sindereve850live@gmail.com>
2026-04-27 19:28:24 +0500

    feat: improving the code for communicating with the training service

--- services/agents/app/services/trainer/__init__.py
+++ services/agents/app/services/trainer/__init__.py
@@ -1,26 +1,20 @@
-import os
-import requests
-from typing import Dict, Any, List
+from typing import Dict, Any
 from crewai.tools import tool
 
+from .utils import *
 from app.logs import get_logger
-from app.config import config_url
 
 logger = get_logger(__name__)
 
 @tool("GetExampleJSONTrainer")
+@handle_errors
 def get_example_run_config_trainer_json() -> Dict[str, Any]:
     """Получить пример JSON для запуска тренировки модели"""
-    try:
-        resp = requests.get(f"{config_url.TRAINER_URL}/info/example_config")
-        data = resp.json()
-        return data
-    except Exception as e:
-        logger.error("Не удалось получить пример конфигураций обучения")
-        return {"error": e}
+    return get_json(f"/info/example_config")
 
 @tool("GetAllAvailableModels")
-def get_type_and_name_models(filter: str) -> List[str]:
+@handle_errors
+def get_type_and_name_models(filter: str) -> Dict[str, Any]:
     """
     Получить все имеющиеся модели в распоряжении.
 
@@ -28,27 +22,10 @@ def get_type_and_name_models(filter: str) -> List[str]:
         filter - фильтр моделей(Пример: "*resnet*" - поиск всех моделей, 
     в которых есть слово resnet)
     """
-    try:
-        params={}
-        params["filter"] = filter
-
-        resp = requests.get(
-            f"{config_url.TRAINER_URL}/info/ml_models",
-            params=params
-        )
-        data = resp.json()
-        return data
-    except Exception as e:
-        logger.error("Не удалось получить информацию об имеющихся моделях")
-        return [f'Ошибка: {e}']
+    return get_json("/info/ml_models",params={"filter": filter})
 
 @tool("GetInfoDevice")
+@handle_errors
 def get_info_device() -> Dict[str, Any]:
     """Получить информацию о технических возможностях обучения"""
-    try:
-        resp = requests.get(f"{config_url.TRAINER_URL}/info/device")
-        data = resp.json()
-        return data
-    except Exception as e:
-        logger.error("Не удалось получить информацию об устройствах")
-        return {"error": e}
+    return get_json("/info/device")
