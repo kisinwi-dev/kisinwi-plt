143eb53d3677c0257c1adb94d2a5cd5d3eff2dca
Sindereve <sindereve850live@gmail.com>
2026-05-05 18:40:53 +0500

    feat: added global error handling 500

--- services/ml_models/main.py
+++ services/ml_models/main.py
@@ -1,21 +1,28 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.health import check_health_all
+from app.api.routers import routers
+from app.logs import get_logger
+
+logger = get_logger(__name__)
 
 # Проверка состояния требуемых БД
 # НЕ БЛОКИРУЕТ ЗАПУСК ЕСЛИ ТРЕБУЕМЫЕ БД НЕ РАБОТАЮТ
 check_health_all()
-
-from app.api.routers import routers
 
 app = FastAPI(
     title="ML Models Service",
     version="0.1.0"
 )
 
+logger.info("✅ app создано")
+
 app.include_router(routers)
+
+logger.info("✅ Добавлены эндпоинты")
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,6 +31,16 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Глобальная неизвестная ошибка: {exc}", exc_info=True)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Неизвестная внутренняя ошибка сервера."}
+    )
+
+logger.info("✅ Добавлены обработчики ошибок")
 
 if __name__ == "__main__":
     uvicorn.run(
