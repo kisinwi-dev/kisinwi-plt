0ccda2ac5531e61211e5239de6175707270aa251
Sindereve <sindereve850live@gmail.com>
2026-05-06 17:14:09 +0500

    feat: added error emissions

--- services/tasker/main.py
+++ services/tasker/main.py
@@ -3,18 +3,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.health import check_health_all
+from app.api.routers import routers
+from app.api.exceptions import setup_exception_handlers
 
 # Проверка состояния требуемых БД
 # НЕ БЛОКИРУЕТ ЗАПУСК ЕСЛИ ТРЕБУЕМЫЕ БД НЕ РАБОТАЮТ
 check_health_all()
-
-from app.api.routers import routers
 
 app = FastAPI(
     title="Tasker Service",
     version="0.1.0"
 )
 
+# Настройка выкидывания ошибок с бд и ошибки 500
+setup_exception_handlers(app)
+
+# добавление роутеров
 app.include_router(routers)
 
 app.add_middleware(
