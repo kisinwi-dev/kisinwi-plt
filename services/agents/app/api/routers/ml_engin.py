d47dbf010754f91b45e94507a08af1bcbe61be75
Sindereve <sindereve850live@gmail.com>
2026-05-13 13:48:47 +0500

    fix: arguments in ml_enginner endpoint

--- services/agents/app/api/routers/ml_engin.py
+++ services/agents/app/api/routers/ml_engin.py
@@ -13,20 +13,20 @@ routers = APIRouter(
 )
 def run_etp(
     discussion_id: str = Query(description="ID дискуссии"),
-    business_goal: str = Query(description="Бизнес требования к модели"),
-    technical_goal: str = Query(description="Технические требования к модели"),
-    training_goal: str = Query(description="Цель обучения"),
-    researcher_info: str = Query(description="Количество инженеров")
+    business_requirements: str = Query(description="Бизнес требования к модели"),
+    deployment_constraints: str = Query(description="Технические требования к модели"),
+    training_objective: str = Query(description="Цель обучения"),
+    researcher_proposals: str = Query(description="Количество инженеров")
 ):
     try:
         
         discussion_context.set(discussion_id)
 
         result = run_ml_engineering(
-            business_goal=business_goal,
-            technical_goal=technical_goal,
-            training_goal=training_goal,
-            researcher_info=researcher_info,
+            business_requirements=business_requirements,
+            deployment_constraints=deployment_constraints,
+            training_objective=training_objective,
+            researcher_proposals=researcher_proposals,
             verbose=True
         )
 
