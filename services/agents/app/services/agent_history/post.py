1e89d0ec7ffd1817aeaccc5aefa47a5f43d53545
Sindereve <sindereve850live@gmail.com>
2026-05-22 12:04:51 +0500

    feat: add post system message

--- services/agents/app/services/agent_history/post.py
+++ services/agents/app/services/agent_history/post.py
@@ -8,9 +8,9 @@ from app.core.memory import discussion_context
 logger = get_logger(__name__)
 
 def add_reponse_in_history(
-        response_id: str,
-        agent_role: str,
-        agent_response: str
+    response_id: str,
+    agent_role: str,
+    agent_response: str
 ) -> bool:
     """
     Добавление ответа агента в историю
@@ -20,31 +20,68 @@ def add_reponse_in_history(
         agent_role: Роль агента
         agent_response: Полученный ответ от агента 
     """
-    discussion_id = discussion_context.get()
 
     agent_info = {}
     agent_info["response_id"] = response_id
     agent_info["text"] = agent_response
     agent_info["agent_role"] = agent_role
 
-    return _post(discussion_id, agent_info)
-
-def _post(
-    discussion_id: str,
-    agent_response: Dict[str, Any]
-) -> bool:
-    """Синхронная отправка ответа агента в сервис истории"""
+    discussion_id = discussion_context.get()
     url = f"{config_url.AGENT_HISTORY}/discussions/{discussion_id}/responses"
     
     try:
         
         result = requests.post(
             url,
-            json=agent_response
+            json=agent_info
         )
         
         if result.status_code == 201:
-            logger.info(f"✅ Ответ агента занесён в историю discussion_id=`{discussion_id}` response=`{agent_response["response_id"]}`")
+            logger.info(f"✅ Ответ агента занесен в историю discussion_id=`{discussion_id}` response_id='{response_id}'")
+            return True
+        else:
+            logger.error(f"Ошибка отправки: {result.status_code} - {result.text}")
+            return False
+
+    except Exception as e:
+        logger.error(f"Непредвиденная ошибка: {e}")
+        return False
+
+def add_system_message(
+    type_: str,
+    message: str
+) -> bool:
+    """
+    Добавление сообщения от системы в историю
+    
+    Args:
+        type_: Тип сообщения
+            На данный момент реализовано несколько типов:
+            * "INFO"
+            * "WARNING"
+            * "ERROR"
+            * "TOOL_CALL"
+            * "TOOL_RESULT"
+            * "AGENT_RESPONSE"
+        message: Сообщение
+    """
+
+    message_info = {}
+    message_info["type_"] = type_
+    message_info["message"] = message
+
+    discussion_id = discussion_context.get()
+    url = f"{config_url.AGENT_HISTORY}/discussions/{discussion_id}/system_messages"
+    
+    try:
+        
+        result = requests.post(
+            url,
+            json=message_info
+        )
+        
+        if result.status_code == 201:
+            logger.info(f"✅ Запись действия занесена в историю discussion_id=`{discussion_id}`")
             return True
         else:
             logger.error(f"Ошибка отправки: {result.status_code} - {result.text}")
