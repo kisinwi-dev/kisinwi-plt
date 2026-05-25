5165b054633a78a2bacf246bddf3da49317bfd17
Sindereve <sindereve850live@gmail.com>
2026-05-25 19:51:52 +0500

    refactor: /health update description

--- services/agents/app/api/routers/__init__.py
+++ services/agents/app/api/routers/__init__.py
@@ -7,9 +7,10 @@ from .full_pipeline import routers as r_fp
 from .info import routers as r_i
 
 routers = APIRouter()
+routers.include_router(r_i)
 routers.include_router(r_a)
 routers.include_router(r_r)
 routers.include_router(r_s)
 routers.include_router(r_ml)
 routers.include_router(r_fp)
-routers.include_router(r_i)
+
